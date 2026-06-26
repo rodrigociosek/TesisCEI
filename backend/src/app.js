3ff308418ad9cfec0cd36901f7d17a918feb4926
Alan151820 <restellialan@gmail.com>
2026-06-26 01:25:44 -0300

    Estructura del proyecto

---
+++ backend/src/app.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.listen(3000, () => {
+  console.log('Servidor corriendo en http://localhost:3000')
+})
