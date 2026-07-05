f813d529723374dd332c673ff57b2c9d071c45cc
Rodrigo <rodrigociosek@gmail.com>
2026-07-05 13:15:34 -0300

    feat(RF-014): implementar creación de producto con validaciones y formulario

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from 'react-router-dom'
+<<<<<<< HEAD
 import Registro from './pages/autenticacion/Registro'
 import Verificar from './pages/autenticacion/Verificar'
 import Login from './pages/autenticacion/Login'
@@ -8,6 +9,13 @@ import NuevaContrasena from './pages/autenticacion/NuevaContrasena'
 import Inicio from './pages/modoDistribuidor/Inicio'
 import PanelDistribuidor from './pages/modoDistribuidor/PanelDistribuidor'
 import FichaProducto from './pages/modoDistribuidor/FichaProducto'
+=======
+import Registro from './pages/Registro'
+import Verificar from './pages/Verificar'
+import Inicio from './pages/Inicio'
+import Login from './pages/Login'
+import FichaProducto from './pages/FichaProducto'
+>>>>>>> e0c674d (feat(RF-014): implementar creación de producto con validaciones y formulario)
 
 function App() {
   return (
@@ -15,11 +23,14 @@ function App() {
       <Route path='/registro' element={<Registro />} />
       <Route path='/verificar' element={<Verificar />} />
       <Route path='/login' element={<Login />} />
+<<<<<<< HEAD
       <Route path='/recuperarContrasena' element={<RecuperarContrasena />} />
       <Route path='/verificarRecuperacion' element={<VerificarRecuperacion />} />
       <Route path='/nuevaContrasena' element={<NuevaContrasena />} />
       <Route path='/inicio' element={<Inicio />} />
       <Route path='/panelDistribuidor' element={<PanelDistribuidor />} />
+=======
+>>>>>>> e0c674d (feat(RF-014): implementar creación de producto con validaciones y formulario)
       <Route path='/producto/nuevo' element={<FichaProducto />} />
     </Routes>
   )
